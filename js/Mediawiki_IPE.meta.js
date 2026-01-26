a034754efbb2d19295f3ce43f0799a61ddb2b8ee
smalllqiang <F11F10E8@outlook.com>
2026-01-26 19:10:56 +0800

    chore: 格式化IPE工具腳本

---
+++ js/Mediawiki_IPE.meta.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name         Mediawiki IPE
+// @version      0.0.1
+// @downloadURL  https://cdn.jsdelivr.net/gh/smalllqiang/tm-user-js-mirror@main/js/Mediawiki_IPE.user.js
+// @updateURL    https://cdn.jsdelivr.net/gh/smalllqiang/tm-user-js-mirror@main/js/Mediawiki_IPE.meta.js
+// ==/UserScript==
---
+++ js/Mediawiki_IPE_NEXT.meta.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name         Mediawiki IPE NEXT
+// @version      0.0.1
+// @downloadURL  https://cdn.jsdelivr.net/gh/smalllqiang/tm-user-js-mirror@main/js/Mediawiki_IPE_NEXT.user.js
+// @updateURL    https://cdn.jsdelivr.net/gh/smalllqiang/tm-user-js-mirror@main/js/Mediawiki_IPE_NEXT.meta.js
+// ==/UserScript==
