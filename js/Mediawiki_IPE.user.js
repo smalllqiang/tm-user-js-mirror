735dddf9a4cc1223164c3fa1a5fdcc94ed2454c4
smalllqiang <F11F10E8@outlook.com>
2026-01-28 01:39:44 +0800

    chore: 添加icon

--- js/Mediawiki_IPE.user.js
+++ js/Mediawiki_IPE.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Mediawiki IPE
 // @namespace    Mediawiki IPE
-// @version      0.0.1
+// @version      0.0.2
 // @description  Mediawiki使用IPE工具
 // @author       dragon-fish, sq修改
 // @match        newmoon.click/*
 // @match        thwiki.cc/*
 // @match        *.moegirl.org.cn/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=www.mediawiki.org
 // @grant        none
 // @downloadURL  https://cdn.jsdelivr.net/gh/smalllqiang/tm-user-js-mirror@main/js/Mediawiki_IPE.user.js
 // @updateURL    https://cdn.jsdelivr.net/gh/smalllqiang/tm-user-js-mirror@main/js/Mediawiki_IPE.meta.js
--- js/Mediawiki_IPE_NEXT.user.js
+++ js/Mediawiki_IPE_NEXT.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Mediawiki IPE NEXT
 // @namespace    Mediawiki IPE NEXT
-// @version      0.0.1
+// @version      0.0.2
 // @description  Mediawiki使用IPE Next工具
 // @author       dragon-fish, sq修改
 // @match        newmoon.click/*
 // @match        thwiki.cc/*
 // @match        *.moegirl.org.cn/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=www.mediawiki.org
 // @grant        none
 // @downloadURL  https://cdn.jsdelivr.net/gh/smalllqiang/tm-user-js-mirror@main/js/Mediawiki_IPE_NEXT.user.js
 // @updateURL    https://cdn.jsdelivr.net/gh/smalllqiang/tm-user-js-mirror@main/js/Mediawiki_IPE_NEXT.meta.js
