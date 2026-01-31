015986314f95f221ea07010ba66ba05c632100c8
smalllqiang <F11F10E8@outlook.com>
2026-01-31 19:10:59 +0800

    feat: 添加www.mediawiki.org

--- js/Mediawiki_IPE_NEXT.user.js
+++ js/Mediawiki_IPE_NEXT.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Mediawiki IPE NEXT
 // @namespace    Mediawiki IPE NEXT
-// @version      0.0.2
+// @version      0.0.3
 // @description  Mediawiki使用IPE Next工具
 // @author       dragon-fish, sq修改
 // @match        newmoon.click/*
 // @match        thwiki.cc/*
 // @match        *.moegirl.org.cn/*
+// @match        https://www.mediawiki.org/wiki/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=www.mediawiki.org
 // @grant        none
 // @downloadURL  https://cdn.jsdelivr.net/gh/smalllqiang/tm-user-js-mirror@main/js/Mediawiki_IPE_NEXT.user.js
