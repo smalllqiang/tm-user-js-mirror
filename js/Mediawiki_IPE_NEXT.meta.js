d2a132d61cb02b3f330b5700e6b354761accd2d7
smalllqiang <F11F10E8@outlook.com>
2026-02-13 19:52:33 +0800

    feat: 添加 thonly.cc

--- js/Mediawiki_IPE_NEXT.meta.js
+++ js/Mediawiki_IPE_NEXT.meta.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Mediawiki IPE NEXT
 // @namespace    https://github.com/smalllqiang
-// @version      0.0.4
+// @version      0.0.5
 // @description  Mediawiki使用IPE Next工具
 // @author       dragon-fish, sq修改
 // @match        newmoon.click/*
 // @match        thwiki.cc/*
 // @match        *.moegirl.org.cn/*
 // @match        https://www.mediawiki.org/wiki/*
+// @match        https://thonly.cc/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=www.mediawiki.org
 // @grant        none
 // @downloadURL  https://cdn.jsdelivr.net/gh/smalllqiang/tm-user-js-mirror@main/js/Mediawiki_IPE_NEXT.user.js
